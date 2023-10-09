992f22ed92d0d42ce7bfd1c7e47ea0c3911578e7
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2023-10-09 15:07:07 +0300

    Added scroll to the latest item

--- presentation/src/commonMain/kotlin/com/idfinance/debugview/presentation/ui/LogView.kt
+++ presentation/src/commonMain/kotlin/com/idfinance/debugview/presentation/ui/LogView.kt
@@ -2,8 +2,10 @@ package com.idfinance.debugview.presentation.ui
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 import com.idfinance.debugview.presentation.decompose.DebugComponent
@@ -11,8 +13,13 @@ import com.idfinance.debugview.presentation.decompose.DebugComponent
 @Composable
 internal fun LogView(component: DebugComponent) {
     val model by component.model.subscribeAsState()
+    val state = rememberLazyListState()
 
-    LazyColumn {
+    LaunchedEffect(model.logs.size) {
+        state.animateScrollToItem(maxOf(model.logs.size - 1, 0))
+    }
+
+    LazyColumn(state = state) {
         items(model.logs) {
             Text(it.message)
         }
