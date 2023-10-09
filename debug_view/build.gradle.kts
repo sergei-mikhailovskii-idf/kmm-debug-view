805ea365e2c7113b1106fbf25d024084f491f2d9
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2023-10-09 17:31:58 +0300

    Modified android publishing

--- debug_view/build.gradle.kts
+++ debug_view/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.idfinance.kmm"
-version = "0.0.1"
+version = "0.0.2"
 
 kotlin {
     androidTarget {
@@ -16,6 +16,7 @@ kotlin {
                 jvmTarget = "1.8"
             }
         }
+        publishLibraryVariants("release", "debug")
     }
     ios()
     iosSimulatorArm64()
